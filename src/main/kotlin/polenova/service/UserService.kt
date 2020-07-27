f0e06e4f740de2d14cd9ed35c454272e84d2c9c8
Marina <56669936+Polenova@users.noreply.github.com>
2020-07-27 15:28:25 +0200

    Update UserService.kt

--- src/main/kotlin/polenova/service/UserService.kt
+++ src/main/kotlin/polenova/service/UserService.kt
@@ -71,9 +71,4 @@ class UserService(
             return AuthenticationResponseDto(token)
         }
     }
-
-    /*@KtorExperimentalAPI
-    suspend fun saveFirebaseToken(id: Long, firebaseToken: String) {
-        repo.saveFirebaseToken(id, firebaseToken) ?: throw NotFoundException()
-    }*/
 }
